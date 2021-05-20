abd90b663f204fe1bd6d37ebcb4ad20cdc9fe14a
Daniel Porteous <danielporteous1@gmail.com>
2021-05-20 14:12:59 -0700

    Validate the requested proxy resource against the allowed providers list

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
+use anyhow::{bail, Error, Result};
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Client, Method, Request, Response, Server, StatusCode, Uri};
-use log::{debug, error, info, warn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode, Uri};
+use log::{debug, info, warn};
 use std::convert::Infallible;
 use std::net::{IpAddr, SocketAddr};
 use structopt::{self, StructOpt};
-
-type HttpClient = Client<hyper::client::HttpConnector>;
 
 #[derive(Debug, StructOpt)]
 pub struct Args {
@@ -23,21 +22,23 @@ pub struct Args {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let args = Args::from_args();
-
     debug!("Running with args: {:#?}", args);
 
-    let client = Client::builder()
-        .http1_title_case_headers(true)
-        .http1_preserve_header_case(true)
-        .build_http();
+    let mut allowed_hosts: Vec<String> = Vec::new();
+    for ap in args.allowed_providers.iter() {
+        match ap.host() {
+            Some(host) => allowed_hosts.push(host.to_string()),
+            None => bail!("Given provider does not have a host: {}", ap),
+        }
+    }
 
     let service = make_service_fn(move |_| {
-        let client = client.clone();
-        async move { Ok::<_, Infallible>(service_fn(move |req| proxy(client.clone(), req))) }
+        let ah = allowed_hosts.clone();
+        async move { Ok::<_, Infallible>(service_fn(move |req| proxy(req, ah.clone()))) }
     });
 
     let socket_address = SocketAddr::from((args.address, args.port));
@@ -48,12 +49,13 @@ async fn main() {
 
     info!("Listening on {}:{}", args.address, args.port);
 
-    if let Err(e) = server.await {
-        error!("Server error: {}", e);
-    }
+    server.await.map_err(|e| Error::new(e))
 }
 
-async fn proxy(_client: HttpClient, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+async fn proxy(
+    req: Request<Body>,
+    allowed_hosts: Vec<String>,
+) -> Result<Response<Body>, hyper::Error> {
     // If the request is not a CONNECT request, return 501.
     if req.method() != Method::CONNECT {
         let mut response = Response::default();
@@ -62,6 +64,29 @@ async fn proxy(_client: HttpClient, req: Request<Body>) -> Result<Response<Body>
             "Rejected request for {} with method {} (should be CONNECT)",
             req.uri(),
             req.method()
+        );
+        return Ok(response);
+    }
+
+    let (_authority, host) = match req.uri().authority() {
+        Some(authority) => (authority.to_string(), authority.host().to_string()),
+        None => {
+            let mut response = Response::default();
+            *response.status_mut() = StatusCode::BAD_REQUEST;
+            warn!(
+                "Requested URI {} does not contain an authority to connect to",
+                req.uri(),
+            );
+            return Ok(response);
+        }
+    };
+
+    if !allowed_hosts.contains(&host) {
+        let mut response = Response::default();
+        *response.status_mut() = StatusCode::BAD_REQUEST;
+        warn!(
+            "Rejected request for {} which is not in the list of allowed providers",
+            host,
         );
         return Ok(response);
     }
