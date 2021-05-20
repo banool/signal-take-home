b83397cd21dfd4778173e791b63424a924309722
Daniel Porteous <danielporteous1@gmail.com>
2021-05-20 12:45:47 -0700

    Add basic argument parsing

---
+++ README.md
@@ -0,0 +1,15 @@
+# Signal Take Home
+
+## Development
+You should have the following available locally:
+- Cargo 1.52 (rustup is the easiest way to get this).
+
+To check the build as you develop, run this:
+```
+cargo check
+```
+
+To run the server, try something like this:
+```
+cargo run -- --address 127.0.0.1 -p 8888 --allowed-providers api.giphy.com
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::Uri;
+use std::net::IpAddr;
+use structopt::{self, StructOpt};
+
+#[derive(Debug, StructOpt)]
+pub struct Args {
+    /// Address to run the server on.
+    #[structopt(long, alias = "ip")]
+    address: IpAddr,
+
+    /// Port to bind to.
+    #[structopt(short, long)]
+    port: u16,
+
+    /// Providers that we will allow users to make requests to via this proxy.
+    #[structopt(long, required = true)]
+    allowed_providers: Vec<Uri>,
+}
+
+
+#[tokio::main]
+async fn main() {
+    let args = Args::from_args();
+
+    println!("args: {:#?}", args);
 }
